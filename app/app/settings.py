fe7fd1ff6459a6c435f1a85e0f48dda3cbe3a1b4
Likeabooom <borkos44@gmail.com>
2020-08-25 17:18:04 +0200

    Debug = Flase

--- app/app/settings.py
+++ app/app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+tb!q0(bc^1l96ox54t-5=@7oix&otlbg(3#=ou)n!poj0&xac'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['checkmodelofyourcar.herokuapp.com','localhost','http://127.0.0.1:8000/']
 
